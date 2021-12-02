2a3dc73b7c32fb660067bd431a19481dc733cb1b
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-02 08:16:04 -0300

    Drawing Boders

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -14,7 +14,17 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+World.add(world, MouseConstraint.create(engine, { 
+  mouse: Mouse.create(render.canvas)
+}))
+
+// Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+World.add(world, walls)
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50))
