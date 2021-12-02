ca17b78f74a31ecbf045f39332897d99fcd25c35
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-02 08:25:07 -0300

    Generating Random Shapes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({ 
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width,
+    height
   }
 });
 
@@ -27,4 +31,20 @@ const walls = [
 ];
 World.add(world, walls)
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50))
+// Random Shapes
+
+for(let i = 0; i<50; i++){
+  if (Math.random() > 0.5) {
+    World.add(world, 
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    )
+  } else {
+    World.add(world, 
+      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+        render: { 
+          fillStyle: 'red'
+        }
+      })
+    )
+  }
+}
