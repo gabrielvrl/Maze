08740aa51325c46f2f8bf720290899221faaa0c5
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-03 08:08:50 -0300

    bastracting Maze Dimensions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 5;
 const width = 600;
 const height = 600;
 
@@ -28,10 +29,11 @@ const walls = [
 World.add(world, walls)
 
 // Maze generation
-const grid = Array(3).fill(null).map(() => Array(3).fill(false))
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false))
 
-const verticals = Array(3).fill(null).map(() => Array(2).fill(false))
-const horizontals = Array(2).fill(null).map(() => Array(3).fill(false))
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
 
+console.log(grid)
 console.log(verticals)
 console.log(horizontals)
