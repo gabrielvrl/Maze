87f21d4ca40f11bb6684bc26aebcb7cf4b7ea172
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-06 08:18:34 -0300

    Handling Keypresses

--- index.js
+++ index.js
@@ -151,4 +151,22 @@ const ball = Bodies.circle(
   unitLength / 2,
   unitLength / 4
 );
-World.add(world,ball)
+World.add(world,ball);
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 87){
+    console.log('move ball up')
+  }
+
+  if (event.keyCode === 68){
+    console.log('move ball right')
+  }
+
+  if (event.keyCode === 83){
+    console.log('move ball down')
+  }
+
+  if (event.keyCode === 65){
+    console.log('move ball left')
+  }
+})
